7baab001e553c1caeec1da3b25d25acd3eab8a85
alexlyee <alexalternateemail@gmail.com>
2019-10-04 21:18:24 -0400

    improved tools to get started quicker

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-echo Don't forget to run install_dep.bat prier to starting^!
+echo Don't forget to run install_dep.bat prior to starting^!
 title MassDM Discord Bot
 REM set "BOT_TOKEN=YOUR TOKEN"
 node app.js --harmony
